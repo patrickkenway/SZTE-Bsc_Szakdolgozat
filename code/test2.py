d354621ebcfd586e850356e73ed44232cd8388b5
Puskás Patrik <h148500@stud.u-szeged.hu>
2025-03-06 10:16:48 +0000

    Replace test2.py

--- code/test2.py
+++ code/test2.py
@@ -7,19 +7,25 @@ SCRENN_HEIGHT = 900
 
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCRENN_HEIGHT))
 
-player = pygame.Rect((300,250,50,50))
+player = pygame.Rect((300,150,50,50))
+
+square1 = pygame.Rect((300, 200, 30, 30))
 
 #-----------------------placeholder---------------------------
 #-----------------------placeholder---------------------------
 
-speed = 5
+speed = 1
 run = True
 while run:
     point = pygame.mouse.get_pos()
     collide = player.collidepoint(point)
+    collide2 = player.colliderect(square1)
     color=(255,0,0) if collide else (255,255,255)
     screen.fill((0,0,0))
+    
+    color=(255,0,0) if collide2 else (255,255,255)
     pygame.draw.rect(screen,color,player)
+    pygame.draw.rect(screen,(255,0,255),square1)
 
     player.clamp_ip(screen.get_rect())
 
