d7ff49aa46be2dde2d0616dea0e4cd1f3eb00f14
Puskas Patrik <h148500@stud.u-szeged.hu>
2025-03-14 10:59:35 +0100

    sokchanges nem tudom miért 375 lol

--- codes/platformer/platformer.py
+++ codes/platformer/platformer.py
@@ -1,9 +1,11 @@
 import os
+import sys
 import random
 import math
 import pygame
 from os import listdir
 from os.path import isfile, join
+
 pygame.init()
 
 pygame.display.set_caption("Platformer")
